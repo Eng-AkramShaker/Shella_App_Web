ab3886dcc3032e4f4d465e11bf9e8a0bc34da75d
maffd17 <maffdmar33@gmail.com>
2025-07-21 03:41:52 +0300

    Applay GetModules on home screen to get section

--- lib/features/home/home/widgets/builds/build_category_list_view.dart
+++ lib/features/home/home/widgets/builds/build_category_list_view.dart
@@ -8,6 +8,7 @@ import 'package:shella_design/common/util/app_styles.dart';
 import 'package:shella_design/features/home/controllers/section_controller.dart';
 import 'package:shella_design/features/home/domain/models/section_model.dart';
 import 'package:shella_design/features/product/widgets/category/category_item.dart';
+import 'package:shella_design/features/search_filter/domain/models/mostSearchedModel/most_searched_model.dart';
 
 class BuildCategoryListView extends StatefulWidget {
   const BuildCategoryListView({super.key});
@@ -23,17 +24,18 @@ class _BuildCategoryListViewState extends State<BuildCategoryListView> {
   Widget build(BuildContext context) {
     return Consumer<SectionProvider>(
       builder: (context, sectionProvider, _) {
-        if (sectionProvider.isLoading) {
+        if (sectionProvider.isLoading2) {
           return Center(child: CircularProgressIndicator());
         }
 
-        if (sectionProvider.errorMessage.isNotEmpty) {
+        if (sectionProvider.errorMessage2.isNotEmpty) {
           return Center(child: Text(sectionProvider.errorMessage));
         }
 
-        final sections = sectionProvider.categories;
-        final SectionModel selectedCategory = sections[selectedIndex];
-        final List<SectionModel> childCategories = selectedCategory.childes;
+      //  final sections = sectionProvider.categories;
+       final modules= sectionProvider.modules;
+        //final SectionModel selectedCategory = sections[selectedIndex];
+      //  final List<SectionModel> childCategories = selectedCategory.childes;
 
         return Directionality(
           textDirection: TextDirection.rtl,
@@ -44,8 +46,8 @@ class _BuildCategoryListViewState extends State<BuildCategoryListView> {
                 scrollDirection: Axis.horizontal,
                 reverse: true,
                 child: Row(
-                  children: List.generate(sections.length, (index) {
-                    final SectionModel cat = sections[index];
+                  children: List.generate(modules.length, (index) {
+                    final Module mod = modules[index];
                     return Padding(
                       padding: EdgeInsets.only(right: 10.w, left: 3.w),
                       child: GestureDetector(
@@ -60,13 +62,15 @@ class _BuildCategoryListViewState extends State<BuildCategoryListView> {
                             mainAxisSize: MainAxisSize.min,
                             children: [
                               CategoryItem(
-                                image: cat.imageFullUrl ?? AppImages.empty,
-                                label: cat.name,
+                             //   image: cat.imageFullUrl ?? AppImages.empty,
+                             image: mod.thumbnailFullUrl ?? AppImages.empty,
+                             label: mod.moduleName ?? "name",
+                              //  label: cat.name,
                               ),
                               SizedBox(height: 7),
                               Container(
                                 height: 3,
-                                width: _getTextWidth(context, cat.name) * 1.3,
+                                width: _getTextWidth(context, mod.moduleName ?? "") * 1.3,
                                 color: selectedIndex == index ? AppColors.greenColor : Colors.transparent,
                               ),
                             ],
