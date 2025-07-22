92a863f14fb484b72749e66c34078e197b9d4237
Raim <ramiturki99@outlook.com>
2025-07-22 16:02:23 +0300

    before merge with assaf

--- lib/features/cart/widgets/mobile/bottom_bar_CartDetails.dart
+++ lib/features/cart/widgets/mobile/bottom_bar_CartDetails.dart
@@ -3,9 +3,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:shella_design/common/widgets/texts/custom_text.dart';
+
 import 'package:shella_design/features/product/widgets/buttons/quantity_selector.dart';
+
+
+
+
+import 'package:shella_design/features/cart/domain/models/cart_model.dart';
+
 import 'package:shella_design/common/util/app_colors.dart';
 import 'package:shella_design/common/util/app_styles.dart';
+
+import '../../controllers/cart_controller.dart';
+import 'builds/build_quantity_controls.dart';
 
 class BottomBar_CartDetails extends StatelessWidget {
   const BottomBar_CartDetails({super.key});
@@ -17,13 +27,18 @@ class BottomBar_CartDetails extends StatelessWidget {
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
+
           QuantitySelector(),
+
+
           Container(
             margin: EdgeInsets.symmetric(horizontal: 16),
             height: 60,
             width: 450.w,
             child: ElevatedButton(
+
               onPressed: () {},
+
               style: ElevatedButton.styleFrom(
                 backgroundColor: AppColors.greenColor,
                 minimumSize: Size(double.infinity, 50),
@@ -48,7 +63,9 @@ class BottomBar_CartDetails extends StatelessWidget {
                   Row(
                     children: [
                       Custom_Text(context,
+
                           text: "50 ريال", style: font14White400W(context)),
+
                       SizedBox(width: 5),
                       Icon(Icons.money, color: AppColors.wtColor, size: 20),
                     ],
